d1dccd479b93bf04023b0732179829fe052890d7
vinaykanth-s <vinaykanth.edu@gmail.com>
2023-02-26 19:28:43 +0530

    knight capture positions highlight handled

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ChessBoard.css'
 
 const ChessBoard = () => {
+  const [selectedCell, setSelectedCell] = useState('')
   const isBlackCell = (row, col) => {
     return (row % 2 === 0 && col % 2 === 0) || (row % 2 === 1 && col % 2 === 1)
   }
 
+  const handleCellClick = (row, col) => {
+    setSelectedCell({ row, col })
+  }
+
+  const isCapturableByKnight = (row, col, selectedRow, selectedCol) => {
+    const rowDiff = Math.abs(row - selectedRow)
+    const colDiff = Math.abs(col - selectedCol)
+    return (rowDiff === 2 && colDiff === 1) || (rowDiff === 1 && colDiff === 2)
+  }
+
   const renderBoard = () => {
     const board = []
-
     for (let row = 0; row < 8; row++) {
       const rowCells = []
 
       for (let col = 0; col < 8; col++) {
         const isBlack = isBlackCell(row, col)
+        const isSelected =
+          selectedCell && selectedCell.row === row && selectedCell.col === col
+        const isCapturable =
+          selectedCell &&
+          isCapturableByKnight(row, col, selectedCell.row, selectedCell.col)
 
         rowCells.push(
           <div
             key={`${row}-${col}`}
-            className={`cell ${isBlack ? 'black' : 'white'}`}
+            className={`cell ${isBlack ? 'black' : 'white'} ${
+              isSelected && 'selected'
+            } ${isCapturable && 'capturable'}`}
+            onClick={() => handleCellClick(row, col)}
           />
         )
       }
